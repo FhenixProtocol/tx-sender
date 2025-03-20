dfc01a54f657526f7947047e768ff0d12f2ef4db
haim <haim@fhenix.io>
2025-03-20 11:50:03 +0200

    Align execution

--- hardhat-eip1559/hardhat.config.js
+++ hardhat-eip1559/hardhat.config.js
@@ -3,25 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv/config");
 const config = {
-    solidity: {
-        version: "0.8.28",
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200,
-            },
-        },
-    },
     networks: {
         hardhat: {
             initialBaseFeePerGas: 1000000000, // Set initial base fee (1 gwei)
             allowUnlimitedContractSize: true, // Avoid contract size issues
             chainId: 420105,
-            hardfork: "london",
+            accounts: [
+                {
+                    privateKey: process.env.HARDHAT_PRIVATE_KEY,
+                    balance: "100000000000000000000", // 100 ETH for testing
+                },
+            ],
             mining: {
                 auto: false, // Disable auto-mining so transactions stay pending
             },
         },
     },
+    solidity: "0.8.28",
 };
 exports.default = config;
